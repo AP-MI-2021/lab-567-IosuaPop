016d41d0467e1c6e8dcbdb1c00bbe674468ec590
Iosua <pop.iosua@yahoo.com>
2022-02-17 01:25:17 +0200

    lab 567

--- Logic/CRUD.py
+++ Logic/CRUD.py
@@ -1,70 +1,101 @@
-from Domain.rezervare import creeazaRezervare, getId
+from Domain.cheltuiala import creeaza_cheltuiala, get_numar_apartament, get_id
 
 
-def adaugaRezervare(id, nume, clasa, pret, checkIn, lista):
-    '''
-    adauga o rezervare intr-o lista
-    :param id: string
-    :param nume: string
-    :param clasa: string
-    :param pret: float
-    :param checkIn: string
-    :param lista: lista de rezervari
-    :return: o lista continand atat elementele vechi, cat si noua rezervare
-    '''
-    ec = "economy"
-    ecp = "economy plus"
-    bs = "business"
-    if getById(id, lista) is not None:
-        raise ValueError("Id-ul exista deja!")
-    '''if clasa != ec or clasa != ecp or clasa != bs:
-        raise ValueError("Clasa de zbor poate fi doar: economy, economy plus sau business!")
-    if checkIn !="da" or checkIn != "nu":
-        raise ValueError("Checkin-ul poate fi doar Da sau Nu")'''
-    rezervare = creeazaRezervare(id, nume, clasa, pret, checkIn)
-    return lista + [rezervare]
+def adauga_cheltuiala(id, numar_apartament, suma, data, tipul, lista, undo_list=None, redo_list=None):
+    """
+    Adauga o cheltuiala intr-o lista
+    :param redo_list: lista pentru redo care e resetata
+    :param undo_list: lista pentru undo care salveaza starea listei inainte de adaugare
+    :param lista: lista de cheltuieli
+    :param id: int
+    :param numar_apartament: string
+    :param suma: float
+    :param data: string
+    :param tipul: string
+    :return: o lista continand atat elemente vechi, cat si noua cheltuiala
+    """
+    if get_by_id(id, lista) is not None:
+        raise ValueError("Id-ul exista deja !")
+    if tipul not in ["intretinere", "canal", "alte cheltuieli"]:
+        raise ValueError("Tipul cheltuielii trebuie sa fie intretinere / canal / alte cheltuieli")
+    if undo_list is not None and redo_list is not None:
+        undo_list.append(lista)
+        redo_list.clear()
+    cheltuiala = creeaza_cheltuiala(id, numar_apartament, suma, data, tipul)
+    return lista + [cheltuiala]
 
-def getById(id, lista):
-    '''
-    gaseste o rezervare cu id-ul dat intr-o lista
-    :param id: string
-    :param lista: lista de rezervari
-    :return: rezervarea cu id-ul dat din lista sau None, daca aceasta nu exista
-    '''
-    for rezervare in lista:
-        if getId(rezervare) == id:
-            return rezervare
+
+def get_by_id(id, lista):
+    """
+    Gaseste o cheltuiala cu id-ul dat intr-o lista
+    :param id: id-ul cheltuielii
+    :param lista: lista de cheltuieli
+    :return: cheltuiala cu id-ul dat din lista, sau None daca aceasta nu exista
+    """
+    for cheltuiala in lista:
+        if get_id(cheltuiala) == id:
+            return cheltuiala
     return None
 
-def stergeRezervare(id, lista):
-    """
-    sterge o rezervare cu id-ul dat din lista
-    :param id: id-ul rezervarii care se va sterge
-    :param lista: lista de rezervari
-    :return: o lista de rezervari fara elementul cu id-ul dat
-    """
-    if getById(id, lista) is None:
-        raise ValueError("Nu exista o rezervare cu id-ul dat!")
-    return [rezervare for rezervare in lista if getId(rezervare) != id]
 
-def modificaRezervare(id, nume, clasa, pret, checkIn, lista):
+def get_by_numar_apartament(numar_apartament, lista):
     """
-    modifica a rezervare cu id-ul dat
-    :param id: id-ul rezervarii
-    :param nume: numele rezervarii
-    :param clasa: clasa rezervarii
-    :param pret: pretul rezervarii
-    :param checkIn: da/nu(daca s-a facut)
-    :param lista: O lista de rezervari.
-    :return: lista modificata.
+    Gaseste o cheltuiala cu numarul de apartament dat intr-o lista
+    :param numar_apartament: numarul de apartament al cheltuielii
+    :param lista: lista de cheltuieli
+    :return: cheltuiala cu numarul de apartament dat din lista, sau None in caz contrar
     """
-    if getById(id, lista) is None:
-        raise ValueError("Nu exista o rezervare cu id-ul dat!")
-    listaNoua = []
-    for rezervare in lista:
-        if getId(rezervare) == id:
-            rezervareNoua = creeazaRezervare(id, nume, clasa, pret, checkIn)
-            listaNoua.append(rezervareNoua)
+    lista_noua = []
+    for cheltuiala in lista:
+        if get_numar_apartament(cheltuiala) == numar_apartament:
+            lista_noua.append(cheltuiala)
+    return lista_noua
+
+
+def sterge_cheltuiala(id, lista, undo_list=None, redo_list=None):
+    """
+    Sterge un apartament cu id-ul dat
+    :param redo_list: lista pentru redo care e resetata
+    :param undo_list: lista pentru undo care salveaza starea listei inainte de stergere
+    :param id: id-ul apartamentului care se va sterge
+    :param lista: lista de cheltuieli
+    :return: o lista de cheltuieli fara cheltuiala care are numarul apartamentului dat
+    """
+    if get_by_id(id, lista) is None:
+        raise ValueError("Nu exista o cheltuiala cu id-ul dat !")
+    if undo_list is not None and redo_list is not None:
+        undo_list.append(lista)
+        redo_list.clear()
+    rezultat = []
+    for cheltuiala in lista:
+        if get_id(cheltuiala) != id:
+            rezultat.append(cheltuiala)
+    return rezultat
+
+
+def modifica_cheltuiala(id, numar_apartament, suma, data, tipul, lista,  undo_list=None, redo_list=None):
+    """
+    Modifica o cheltuiala cu id-ul dat
+    :param redo_list: lista pentru redo care e resetata
+    :param undo_list: lista pentru undo care salveaza lista inainte de modificare
+    :param id: id-ul cheltuielii
+    :param numar_apartament: numarul de apartament al cheltuielii
+    :param suma: suma cheltuielii
+    :param data: data cheltuielii
+    :param tipul: tipul cheltuielii
+    :param lista: lista cu cheltuieli
+    :return: lista modificata
+    """
+    if get_by_id(id, lista) is None:
+        raise ValueError("Nu exista o cheltuiala cu id-ul dat !")
+    if undo_list is not None and redo_list is not None:
+        undo_list.append(lista)
+        redo_list.clear()
+    lista_noua = []
+    for cheltuiala in lista:
+        if get_id(cheltuiala) == id:
+            cheltuiala_noua = creeaza_cheltuiala(id, numar_apartament, suma, data, tipul)
+            lista_noua.append(cheltuiala_noua)
         else:
-            listaNoua.append(rezervare)
-    return listaNoua
+            lista_noua.append(cheltuiala)
+    return lista_noua
--- main.py
+++ main.py
@@ -1,14 +1,36 @@
-from Logic.CRUD import adaugaRezervare
-from Tests.testAll import runAllTests
-from UserInterface.console import runMenu
+"""
+Scrieți un program pentru gestionarea unei asociații de proprietari. Vor fi suportate operațiile:
+1.1. Adăugare / ștergere / modificare cheltuială: se efectuează pe bază de număr de apartament.
+    O cheltuială conține număr apartament, suma, data (DD.MM.YYYY) și tipul: întreținere, canal, alte cheltuieli.
+1.2. Ștergerea tuturor cheltuielilor pentru un apartament dat.
+1.3. Adunarea unei valori la toate cheltuielile dintr-o dată citită.
+1.4. Determinarea celei mai mari cheltuieli pentru fiecare tip de cheltuială.
+1.5. Ordonarea cheltuielilor descrescător după sumă.
+1.6. Afișarea sumelor lunare pentru fiecare apartament.
+1.7. Undo.
+"""
+from Tests.test_all import test_all
+from UI.console import run_menu
+from UI.console_line_command import run_new_menu
 
 
 def main():
-    runAllTests()
+    test_all()
     lista = []
-    lista = adaugaRezervare("1", "Constanta", "economy", float(50), "Nu", lista)
-    lista = adaugaRezervare("2", "Timisoara", "economy", float(30), "Da", lista)
-    runMenu(lista)
+    while True:
+        print("1. Consola normala.")
+        print("2. Console line command.")
+        print("x. Iesire.")
+        optiune = input("Dati optiunea: ")
+        if optiune == "1":
+            run_menu(lista)
+        elif optiune == "2":
+            run_new_menu(lista)
+        elif optiune.lower() == "x":
+            break
+        else:
+            print("Optiune gresita ! Reincercati ")
 
 
-main()
+if __name__ == "__main__":
+    main()
